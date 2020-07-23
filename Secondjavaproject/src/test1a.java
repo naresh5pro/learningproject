16595ebada7801bb836d4636aa2514e044376cdd
Purnachander <ramagiri.purnachander@gmail.com>
2020-07-23 18:00:43 +0530

    first commit

---
+++ Secondjavaproject/src/test1a.java
@@ -0,0 +1,15 @@
+
+public class test1a {
+
+	public static void main(String[] args) {
+		int x,y = 1;
+		x= 10;
+		if(x != 10 && y/0 == 0) {
+System.out.println(y);			
+		}
+		else {
+			System.out.println(y++);	
+		}
+	}
+
+}
