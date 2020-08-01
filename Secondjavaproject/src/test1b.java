6b627fd5cf1c6e435e3e0743c7349b72147f8e22
Purnachander <ramagiri.purnachander@gmail.com>
2020-08-01 21:34:26 +0530

    second commit

--- Secondjavaproject/src/test1b.java
+++ Secondjavaproject/src/test1b.java
@@ -2,7 +2,12 @@
 public class test1b {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int sum , j = 1 ;
+		for (int i = 0 ; i < 5 ; i++) 
+		{
+			sum = i;
+			System.out.println(sum / j + "  hello");
+		}
 
 	}
 
